e886d8d634dbe658de9b06f3d436bbbd4bd85f28
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2022-04-17 22:40:04 +0200

    Upgrade to Java 11, GWT 2.9 and rxgwt 2.3

--- src/main/java/breakout/client/Breakout.java
+++ src/main/java/breakout/client/Breakout.java
@@ -7,17 +7,17 @@ import static io.reactivex.Observable.empty;
 import static io.reactivex.Observable.just;
 import static io.reactivex.Observable.merge;
 import static java.lang.Double.NaN;
-import static org.jboss.gwt.elemento.core.EventType.keydown;
-import static org.jboss.gwt.elemento.core.EventType.keyup;
-import static org.jboss.gwt.elemento.core.EventType.touchcancel;
-import static org.jboss.gwt.elemento.core.EventType.touchend;
-import static org.jboss.gwt.elemento.core.EventType.touchmove;
-import static org.jboss.gwt.elemento.core.EventType.touchstart;
+import static org.jboss.elemento.EventType.keydown;
+import static org.jboss.elemento.EventType.keyup;
+import static org.jboss.elemento.EventType.touchcancel;
+import static org.jboss.elemento.EventType.touchend;
+import static org.jboss.elemento.EventType.touchmove;
+import static org.jboss.elemento.EventType.touchstart;
 
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.core.JsDate;
+import elemental2.dom.BaseRenderingContext2D.FillStyleUnionType;
 import elemental2.dom.CanvasRenderingContext2D;
-import elemental2.dom.CanvasRenderingContext2D.FillStyleUnionType;
 import elemental2.dom.HTMLCanvasElement;
 import elemental2.dom.HTMLPreElement;
 import elemental2.media.AudioBufferSourceNode;
@@ -36,6 +36,7 @@ import java.util.function.IntConsumer;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import jsinterop.base.Js;
 
 /**
@@ -49,8 +50,10 @@ public class Breakout implements EntryPoint {
 
         // Graphics
 
-        HTMLCanvasElement canvas = Js.cast(document.getElementById("stage"));
-        CanvasRenderingContext2D context = Js.cast(canvas.getContext("2d"));
+        @Nullable HTMLCanvasElement canvas = Js.cast(document.getElementById("stage"));
+        if (canvas == null) throw new RuntimeException("canvas not available");
+        @Nullable CanvasRenderingContext2D context = Js.cast(canvas.getContext("2d"));
+        if (context == null) throw new RuntimeException("2d context not available");
         HTMLPreElement debug = Js.cast(document.getElementById("state"));
         boolean mobile = window.navigator.userAgent.contains("Mobi");
 
@@ -68,29 +71,29 @@ public class Breakout implements EntryPoint {
         Runnable drawTitle = () -> {
             context.textAlign = "center";
             context.font = "24px Courier New";
-            context.fillText("Breakout (RxJava + GWT)", canvas.width / 2, canvas.height / 2 - 24);
+            context.fillText("Breakout (RxJava + GWT)", canvas.width / 2d, canvas.height / 2d - 24);
         };
 
         Runnable drawControls = () -> {
             context.textAlign = "center";
             context.font = "16px Courier New";
-            context.fillText("press [<] and [>] to play", canvas.width / 2, canvas.height / 2);
+            context.fillText("press [<] and [>] to play", canvas.width / 2d, canvas.height / 2d);
         };
 
         Consumer<String> drawGameOver = text -> {
-            context.clearRect(canvas.width / 4, canvas.height / 3, canvas.width / 2, canvas.height / 3);
+            context.clearRect(canvas.width / 4d, canvas.height / 3d, canvas.width / 2d, canvas.height / 3d);
             context.textAlign = "center";
             context.font = "24px Courier New";
-            context.fillText(text, canvas.width / 2, canvas.height / 2);
+            context.fillText(text, canvas.width / 2d, canvas.height / 2d);
             context.font = "16px Courier New";
-            context.fillText("press [space] to play again", canvas.width / 2, canvas.height / 2 + 24);
+            context.fillText("press [space] to play again", canvas.width / 2d, canvas.height / 2d + 24);
         };
 
         Runnable drawAuthor = () -> {
             context.textAlign = "center";
             context.font = "14px Courier New";
-            context.fillText("GWT version by Ignacio Baca", canvas.width / 2, canvas.height / 2 + 24);
-            context.fillText("JS version by Manuel Wieser", canvas.width / 2, canvas.height / 2 + 40);
+            context.fillText("GWT version by Ignacio Baca", canvas.width / 2d, canvas.height / 2d + 24);
+            context.fillText("JS version by Manuel Wieser", canvas.width / 2d, canvas.height / 2d + 40);
         };
 
         IntConsumer drawScore = score -> {
@@ -102,7 +105,7 @@ public class Breakout implements EntryPoint {
         DoubleConsumer drawPaddle = position -> {
             context.beginPath();
             context.rect(
-                    position - PADDLE_WIDTH / 2,
+                    position - PADDLE_WIDTH / 2d,
                     context.canvas.height - PADDLE_HEIGHT,
                     PADDLE_WIDTH,
                     PADDLE_HEIGHT
@@ -121,8 +124,8 @@ public class Breakout implements EntryPoint {
         }
 
         class Ball {
-            Position position = new Position();
-            Position direction = new Position();
+            final Position position = new Position();
+            final Position direction = new Position();
             @Override public String toString() {
                 return "Ball{position=" + position + ", direction=" + direction + '}';
             }
@@ -145,8 +148,8 @@ public class Breakout implements EntryPoint {
         Consumer<Brick> drawBrick = brick -> {
             context.beginPath();
             context.rect(
-                    brick.x - brick.width / 2,
-                    brick.y - brick.height / 2,
+                    brick.x - brick.width / 2d,
+                    brick.y - brick.height / 2d,
                     brick.width,
                     brick.height
             );
@@ -169,7 +172,7 @@ public class Breakout implements EntryPoint {
             oscillator.type = "square";
 
             // https://en.wikipedia.org/wiki/Piano_key_frequencies
-            oscillator.frequency.value = Math.pow(2, (key - 49) / 12) * 440;
+            oscillator.frequency.value = Math.pow(2, (key - 49) / 12d) * 440;
 
             //XXX OscillatorNode should implements AudioScheduledSourceNode but it doesn't exists!
             //XXX buffered contains the start/stop methods, but requires unchecked cast
@@ -183,8 +186,8 @@ public class Breakout implements EntryPoint {
         int TICKER_INTERVAL = 17;
 
         class Tick {
-            double time; // millis
-            double delta; // seconds
+            final double time; // millis
+            final double delta; // seconds
             public Tick(double time, double delta) {
                 this.time = time;
                 this.delta = delta;
@@ -195,7 +198,7 @@ public class Breakout implements EntryPoint {
         Observable<Tick> ticker$ = Observable
                 .interval(TICKER_INTERVAL, TimeUnit.MILLISECONDS) //TODO Rx.Scheduler.requestAnimationFrame
                 .map(n -> new Tick(JsDate.now(), NaN))
-                .scan((previous, current) -> new Tick(current.time, (current.time - previous.time) / 1000))
+                .scan((previous, current) -> new Tick(current.time, (current.time - previous.time) / 1000d))
                 .skip(1/*ignore initial value*/);
 
         // Paddle
@@ -217,7 +220,7 @@ public class Breakout implements EntryPoint {
 
         Observable<Double> keyboardPosition$ = ticker$
                 .withLatestFrom(direction$, (ticker, direction) -> direction * ticker.delta * PADDLE_SPEED)
-                .scan(canvas.width / 2., (position, move) -> position + move).skip(1/*ignore initial value*/);
+                .scan(canvas.width / 2d, Double::sum).skip(1/*ignore initial value*/);
 
         Observable<Double> touchPosition$ = fromEvent(document, touchstart)
                 .switchMap(start -> fromEvent(document, touchmove)
@@ -225,18 +228,18 @@ public class Breakout implements EntryPoint {
                         .takeUntil(merge(fromEvent(document, touchcancel), fromEvent(document, touchend))));
 
         Observable<Double> paddle$ = merge(keyboardPosition$, touchPosition$)
-                .map(position -> Math.max(Math.min(position, canvas.width - PADDLE_WIDTH / 2), PADDLE_WIDTH / 2))
+                .map(position -> Math.max(Math.min(position, canvas.width - PADDLE_WIDTH / 2d), PADDLE_WIDTH / 2d))
                 .distinctUntilChanged();
 
         // Bricks
 
         Supplier<Brick[]> factory = () -> {
-            double width = (canvas.width - BRICK_GAP - BRICK_GAP * BRICK_COLUMNS) / BRICK_COLUMNS;
+            double width = (canvas.width - BRICK_GAP - BRICK_GAP * BRICK_COLUMNS) / (double) BRICK_COLUMNS;
             return IntStream.range(0, BRICK_ROWS).boxed().flatMap(i -> {
                 return IntStream.range(0, BRICK_COLUMNS).mapToObj(j -> {
                     Brick brick = new Brick();
-                    brick.x = j * (width + BRICK_GAP) + width / 2 + BRICK_GAP;
-                    brick.y = i * (BRICK_HEIGHT + BRICK_GAP) + BRICK_HEIGHT / 2 + BRICK_GAP + 20;
+                    brick.x = j * (width + BRICK_GAP) + width / 2d + BRICK_GAP;
+                    brick.y = i * (BRICK_HEIGHT + BRICK_GAP) + BRICK_HEIGHT / 2d + BRICK_GAP + 20;
                     brick.width = width;
                     brick.height = BRICK_HEIGHT;
                     return brick;
@@ -245,10 +248,10 @@ public class Breakout implements EntryPoint {
         };
 
         BiPredicate<Brick, Ball> collision = (brick, ball) -> {
-            return ball.position.x + ball.direction.x > brick.x - brick.width / 2
-                    && ball.position.x + ball.direction.x < brick.x + brick.width / 2
-                    && ball.position.y + ball.direction.y > brick.y - brick.height / 2
-                    && ball.position.y + ball.direction.y < brick.y + brick.height / 2;
+            return ball.position.x + ball.direction.x > brick.x - brick.width / 2d
+                    && ball.position.x + ball.direction.x < brick.x + brick.width / 2d
+                    && ball.position.y + ball.direction.y > brick.y - brick.height / 2d
+                    && ball.position.y + ball.direction.y < brick.y + brick.height / 2d;
         };
 
         // Ball
@@ -273,7 +276,7 @@ public class Breakout implements EntryPoint {
         class State {
             Tick tick;
             double paddle;
-            Ball ball = new Ball();
+            final Ball ball = new Ball();
             Brick[] bricks;
             Collisions collisions = new Collisions();
             int score;
@@ -293,8 +296,8 @@ public class Breakout implements EntryPoint {
         }
 
         State INITIAL_OBJECTS = new State();
-        INITIAL_OBJECTS.ball.position.x = canvas.width / 2;
-        INITIAL_OBJECTS.ball.position.y = canvas.height / 2;
+        INITIAL_OBJECTS.ball.position.x = canvas.width / 2d;
+        INITIAL_OBJECTS.ball.position.y = canvas.height / 2d;
         INITIAL_OBJECTS.ball.direction.x = 2;
         INITIAL_OBJECTS.ball.direction.y = 2;
         INITIAL_OBJECTS.bricks = factory.get();
@@ -306,7 +309,7 @@ public class Breakout implements EntryPoint {
         }
 
         Observable<State> state$ = ticker$
-                .withLatestFrom(paddle$, (ticker, paddle) -> new TickerPaddle(ticker, paddle))
+                .withLatestFrom(paddle$, TickerPaddle::new)
                 .scan(INITIAL_OBJECTS, (state, tickerPaddle) -> {
                     final State out = new State();
                     out.tick = tickerPaddle.ticker;
@@ -331,9 +334,9 @@ public class Breakout implements EntryPoint {
                         }
                     }).toArray(Brick[]::new);
 
-                    double paddleTop = canvas.height - PADDLE_HEIGHT - BALL_RADIUS / 2;
-                    if (out.ball.position.x > tickerPaddle.paddle - PADDLE_WIDTH / 2
-                            && out.ball.position.x < tickerPaddle.paddle + PADDLE_WIDTH / 2
+                    double paddleTop = canvas.height - PADDLE_HEIGHT - BALL_RADIUS / 2d;
+                    if (out.ball.position.x > tickerPaddle.paddle - PADDLE_WIDTH / 2d
+                            && out.ball.position.x < tickerPaddle.paddle + PADDLE_WIDTH / 2d
                             && out.ball.position.y > paddleTop) {
                         out.ball.position.y = paddleTop;
                         out.ball.direction.y *= -1;
